0f04536a74f785ce3349910b7107728f1957117c
Ciallo <dison2233@126.com>
2026-03-19 18:19:50 +0800

    disable movement unlocker

--- src/surf/misc/mvunlock.cpp
+++ src/surf/misc/mvunlock.cpp
@@ -11,5 +11,6 @@ void CMovementUnlocker::OnPluginStart() {
 	static auto fn = GAMEDATA::GetMemSig("ServerMovementUnlock");
 	SDK_ASSERT(fn);
 
-	WIN_LINUX(MEM::PatchAddress(fn, 0xEB), MEM::PatchAddress(fn, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90));
+	// FIXME
+	//WIN_LINUX(MEM::PatchAddress(fn, 0xEB), MEM::PatchAddress(fn, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90));
 }
